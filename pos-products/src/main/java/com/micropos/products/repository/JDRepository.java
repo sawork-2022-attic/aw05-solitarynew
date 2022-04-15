989b0b1f976d2e3e86064e0b61d2b3f094320579
solitarynew <1348288404@qq.com>
2022-04-15 21:04:56 +0800

    整合ehcache缓存

--- pos-products/src/main/java/com/micropos/products/repository/JDRepository.java
+++ pos-products/src/main/java/com/micropos/products/repository/JDRepository.java
@@ -5,6 +5,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 
 import java.io.IOException;
@@ -16,6 +17,7 @@ import java.util.List;
 public class JDRepository implements ProductRepository {
     private List<Product> products = null;
 
+    @Cacheable(value="pos-products", key="'allProducts'")
     @Override
     public List<Product> allProducts() {
         try {
@@ -27,6 +29,7 @@ public class JDRepository implements ProductRepository {
         return products;
     }
 
+    @Cacheable(value="pos-products", key="#productId")
     @Override
     public Product findProduct(String productId) {
         for (Product p : allProducts()) {
