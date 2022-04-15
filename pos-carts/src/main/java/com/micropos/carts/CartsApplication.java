b269f820c727cefd0c5b607bafb0fe4ee33c0b1f
solitarynew <1348288404@qq.com>
2022-04-15 20:43:19 +0800

    添加断路器

--- pos-carts/src/main/java/com/micropos/carts/CartsApplication.java
+++ pos-carts/src/main/java/com/micropos/carts/CartsApplication.java
@@ -1,11 +1,18 @@
 package com.micropos.carts;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -19,5 +26,40 @@ public class CartsApplication {
     RestTemplate restTemplate() {
         return new RestTemplate();
 
+    }
+
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> globalCustomConfiguration() {
+        TimeLimiterConfig timeLimiterConfig = TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build();
+        CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom().failureRateThreshold(50)
+                .waitDurationInOpenState(Duration.ofMillis(1000)).slidingWindowSize(2).build();
+
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .timeLimiterConfig(timeLimiterConfig).circuitBreakerConfig(circuitBreakerConfig).build());
+    }
+
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> specificCustomConfiguration1() {
+
+        TimeLimiterConfig timeLimiterConfig = TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build();
+        CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom().failureRateThreshold(50)
+                .waitDurationInOpenState(Duration.ofMillis(1000)).slidingWindowSize(2).build();
+
+        return factory -> factory.configure(builder -> builder.circuitBreakerConfig(circuitBreakerConfig)
+                .timeLimiterConfig(timeLimiterConfig).build(), "circuitBreaker");
+    }
+
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> specificCustomConfiguration2() {
+
+        TimeLimiterConfig timeLimiterConfig = TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build();
+        CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom().failureRateThreshold(50)
+                .waitDurationInOpenState(Duration.ofMillis(1000)).slidingWindowSize(2).build();
+
+        return factory -> factory
+                .configure(
+                        builder -> builder.circuitBreakerConfig(circuitBreakerConfig)
+                                .timeLimiterConfig(timeLimiterConfig).build(),
+                        "circuitBreaker1", "circuitBreaker2", "circuitBreaker3");
     }
 }
--- pos-carts/src/main/java/com/micropos/carts/service/CartServiceImpl.java
+++ pos-carts/src/main/java/com/micropos/carts/service/CartServiceImpl.java
@@ -4,6 +4,8 @@ import com.micropos.carts.model.Cart;
 import com.micropos.carts.model.Item;
 import com.micropos.carts.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
@@ -18,14 +20,23 @@ public class CartServiceImpl implements CartService {
     @LoadBalanced
     protected RestTemplate restTemplate;
 
+    @Autowired
+    private CircuitBreakerFactory circuitBreakerFactory;
+
     @Override
     public Cart add(Cart cart, String productId, int amount) {
-        ResponseEntity<Product> productResponseEntity = restTemplate.getForEntity("http://pos-products/api/products/" + productId, Product.class);
-        //Product product = new Product(productId, "", 0, "");
-        Product product = productResponseEntity.getBody();
-        if (product == null) return cart;
-
-        cart.addItem(new Item(product, amount));
-        return cart;
+        CircuitBreaker circuitBreaker = circuitBreakerFactory.create("circuitbreaker");
+        return circuitBreaker.run(() -> {
+            ResponseEntity<Product> productResponseEntity = restTemplate.
+                    getForEntity("http://pos-products/api/products/" + productId, Product.class);
+            //Product product = new Product(productId, "", 0, "");
+            Product product = productResponseEntity.getBody();
+            if (product == null) return cart;
+            cart.addItem(new Item(product, amount));
+            return cart;
+        }, ex -> {
+            cart.addItem(new Item(new Product(productId, "服务器出故障了，只能这样提醒你了", 0, ""), amount));
+            return cart;
+        });
     }
 }
